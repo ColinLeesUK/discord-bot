2f4315fbd53bac0ce77033c57d93a9d7770a14e2
ColinLeesUK <colinleesa7x+github@gmail.com>
2018-09-02 22:32:19 +0100

    Add message ability when user joins channel

--- bot.js
+++ bot.js
@@ -7,23 +7,43 @@ const bot = new Discord.Client()
 bot.on('ready', function(evt) {
   console.log('Connected')
   console.log('Logged in as: ')
-  console.log(bot.username + ' - (' + bot.id + ')')
+  console.log(bot.user.username + ' - (' + bot.user.id + ')')
 })
 
-bot.on('message', function(user, userID, channelID, message, evt) {
-  if (message.substring(0, 1) == config.prefix) {
-    let args = message.substring(1).split(' ')
+bot.on('message', message => {
+  if (message.content.substring(0, 1) == config.prefix) {
+    let args = message.content.substring(1).split(' ')
     const cmd = args[0]
 
     args = args.splice(1)
 
     switch (cmd) {
       case 'ping':
-        bot.sendMessage({ to: channelID, message: 'Pong!' })
+        if (message.member.user.username === 'Lemming') {
+          message.channel.send('Oh Herro!')
+        } else {
+          message.channel.send('Pong!')
+        }
+        break
+      case 'avatar':
+        message.channel.send(message.author.avatarURL)
         break
       default:
-        bot.sendMessage({ to: channelID, message: 'Unknown command.' })
+        message.channel.send("I'm sorry I didn't understand that...")
     }
+  }
+})
+
+bot.on('voiceStateUpdate', (oldMember, newMember) => {
+  let oldMemberChannel = oldMember.voiceChannel
+  let newMemberChannel = newMember.voiceChannel
+  console.log('Voice status update')
+
+  if(oldMemberChannel === undefined && newMemberChannel !== undefined) {
+    console.log('User ' + newMember.user.username + ' joined!')
+    newMember.send('Oh Herro!')
+  } else if(newMemberChannel === undefined){
+    
   }
 })
 
